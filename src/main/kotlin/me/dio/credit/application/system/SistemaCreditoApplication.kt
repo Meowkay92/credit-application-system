8b01b73e8f10d7c4962dc6b12928eae7d4e6e63b
Cleberton Goncalves <gustavovitorn@gmail.com>
2023-12-26 17:42:31 -0300

    em progresso

---
+++ src/main/kotlin/me/dio/credit/application/system/SistemaCreditoApplication.kt
@@ -0,0 +1,11 @@
+package me.dio.credit.application.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SistemaCreditoApplication
+
+ fun main(args: Array<String>) {
+	runApplication<SistemaCreditoApplication>(*args)
+}
