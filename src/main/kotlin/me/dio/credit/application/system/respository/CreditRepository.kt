4c6ca2275112b1cbe616b9a0a7bd9693078250d7
Cleberton Goncalves <gustavovitorn@gmail.com>
2023-12-27 09:13:22 -0300

    Em Progresso

--- src/main/kotlin/me/dio/credit/application/system/respository/CreditRepository.kt
+++ src/main/kotlin/me/dio/credit/application/system/respository/CreditRepository.kt
@@ -2,7 +2,13 @@ package me.dio.credit.application.system.respository
 
 import me.dio.credit.application.system.entity.Credit
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CreditRepository: JpaRepository<Credit, Long>
+interface CreditRepository: JpaRepository<Credit, Long> {
+    fun findByCreditCode(creditCode: java.util.UUID) : Credit?
+
+    @Query(value = "SELECT * FROM CREDIT WHERE CUSTOMER_ID = ?1", nativeQuery = true)
+    fun findAllByCustomerId(customerId: Long): List<Credit>
+}
